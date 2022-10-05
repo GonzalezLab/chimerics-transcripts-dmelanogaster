40133ee6fc9aa1ea7ee119764357d00445072105
Marta Coronado <marta.coronadoz@gmail.com>
2022-10-05 17:12:28 +0200

    upload

--- 4. Splice sites motif/meme-suite.sh
+++ 4. Splice sites motif/meme-suite.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 meme AGsites.fasta -dna -oc AGmotif -nostatus -time 18000 -mod zoops -nmotifs 1 -minw 6 -maxw 12 -objfun classic -revcomp -markov_order 0
 meme GTsites.fasta -dna -oc GTmotif -nostatus -time 18000 -mod zoops -nmotifs 1 -minw 6 -maxw 12 -objfun classic -revcomp -markov_order 0
